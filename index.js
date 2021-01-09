0cb6c9a9bd70412ee57223189dd6c96eccf59d8a
xuniaoji <xuniaoji@163.com>
2021-01-09 17:24:20 +0800

    handle wrong

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-"use strict";
-const alfy = require("alfy");
+'use strict';
+const alfy = require('alfy');
 // const inputsSF1045668055288 = alfy.input.split(',');
 // const number = inputs[0];
 // const mobile = inputs[1];
-const data = await alfy.fetch("https://api.binstd.com/express/query", {
-  query: {
-    appkey: "9840c89040f56003",
-    type: "auto",
-    number: alfy.input,
-    // mobile, 
-  },
-});
+(async () => {
+  const data = await alfy.fetch('https://api.binstd.com/express/query', {
+    query: {
+      appkey: '9840c89040f56003',
+      type: 'auto',
+      number: alfy.input,
+      // mobile,
+    },
+  });
 
-const items = data.result.list.map((one) => {
-  return {
-    title: one.time,
-    subtitle: one.status,
-  };
-});
+  let items;
+  if (data.result) {
+    items = data.result.list.map((one) => {
+      return {
+        title: one.time,
+        subtitle: one.status,
+      };
+    });
+    const firstLine = data.result.deliverystatus === 3 ? '已签收' : '在路上';
+    items.unshift({ title: data.result.typename, subtitle: firstLine });
+  } else {
+    items = [
+      {
+        title: data.msg,
+      },
+    ];
+  }
 
-const isDelivered = data.result.list.some((one) => one.status.includes('已签收'));
-const firstLine = isDelivered? '已签收' : '在路上';
-
-items.unshift({ title: data.result.typename, subtitle: firstLine });
-
-alfy.output(items);
+  alfy.output(items);
+})();
